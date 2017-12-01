f2307a2bc2343220847665f6a107f59766f7a1c9
Jack Stouffer <jack@jackstouffer.com>
2017-12-01 13:21:39 -0500

    String construction is now working

--- source/stdxdecimal/package.d
+++ source/stdxdecimal/package.d
@@ -51,18 +51,20 @@ public:
         import std.algorithm.searching : all, startsWith;
         import std.utf : byCodeUnit;
         import std.algorithm.comparison : equal;
-        import std.algorithm.iteration : map;
+        import std.algorithm.iteration : map, filter;
         import std.ascii : toLower, isDigit;
         import std.utf : byChar;
+        import std.array : array;
 
         static bool asciiCmp(S1)(S1 a, string b)
         {
             return a.map!toLower.equal(b.byChar.map!toLower);
         }
 
+        // valid characters in non-special number strings
         static bool charCheck(char a)
         {
-            return isDigit(a) || a == 'E' || a == '-' || a == '+' || a == '.';
+            return isDigit(a) || a == 'E' || a == 'e' || a == '-' || a == '+' || a == '.';
         }
 
         auto codeUnits = str.byCodeUnit;
@@ -76,7 +78,6 @@ public:
         immutable frontResult = codeUnits.front;
         if (frontResult == '+')
         {
-            sign = 0;
             codeUnits.popFront;
         }
         if (frontResult == '-')
@@ -99,6 +100,7 @@ public:
             return;
         }
 
+        // having numbers after nan is valid in the spec
         if (codeUnits.save.map!toLower.startsWith("qnan") ||
             codeUnits.save.map!toLower.startsWith("nan"))
         {
@@ -112,11 +114,99 @@ public:
             return;
         }
 
-        if (!all!(a => charCheck(a))(codeUnits.save))
+        bool sawDecimal = false;
+        bool sawExponent = false;
+        bool sawExponentSign = false;
+        byte exponentSign;
+        long sciExponent = 0;
+        auto saved = codeUnits.save;
+        for (; !saved.empty; saved.popFront)
         {
-            qNaN = true;
-            return;
+            auto digit = saved.front;
+
+            if (!charCheck(digit))
+                goto Lerr;
+
+            if (isDigit(digit))
+            {
+                if (!sawExponent)
+                {
+                    coefficient *= 10;
+                    coefficient += cast(uint) (digit - '0');
+                }
+
+                if (sawDecimal && !sawExponent)
+                    exponent--;
+
+                if (sawExponent)
+                {
+                    while (!saved.empty)
+                    {
+                        if (!isDigit(saved.front))
+                            goto Lerr;
+
+                        sciExponent += cast(uint) (saved.front - '0');
+                        if (!saved.empty)
+                        {
+                            saved.popFront;
+                            if (!saved.empty)
+                                sciExponent *= 10;
+                        }
+                    }
+
+                    if (sawExponentSign && exponentSign == -1)
+                        sciExponent *= -1;
+
+                    exponent += sciExponent;
+
+                    if (saved.empty)
+                        return;
+                }
+            }
+
+            if (digit == '+' || digit == '-')
+            {
+                // already have exponent sign, bad input so cancel out
+                if (sawExponentSign)
+                    goto Lerr;
+
+                if (sawExponent)
+                {
+                    if (digit == '-')
+                        exponentSign = -1;
+                    sawExponentSign = true;
+                }
+                else
+                { // no exponent yet, bad input so cancel out
+                    goto Lerr;
+                }
+            }
+
+            if (digit == '.')
+            {
+                // already have decimal, bad input so cancel out
+                if (sawDecimal)
+                    goto Lerr;
+
+                sawDecimal = true;
+            }
+
+            if (digit.toLower == 'e')
+            {
+                // already have exponent, bad input so cancel out
+                if (sawExponent)
+                    goto Lerr;
+
+                sawExponent = true;
+            }
         }
+        return;
+
+        Lerr:
+            qNaN = true;
+            coefficient = 0;
+            exponent = 0;
+            return;
     }
 
     bool opEquals(T)(T f) if (isFloatingPoint!T)
@@ -174,8 +264,8 @@ unittest
     auto nonspecialTestValues = [
         Test("1.0", 0, 10, -1),
         Test("0E+7", 0, 0, 7),
-        Test("-0E-7", 1, 0, 7),
-        Test("1.23E3", 1, 123, 1),
+        Test("-0E-7", 1, 0, -7),
+        Test("1.23E3", 0, 123, 1),
         Test("0001.0000", 0, 10000, -4),
         Test("-10.0004", 1, 100004, -4),
         Test("+15", 0, 15, 0),
@@ -193,16 +283,20 @@ unittest
         SpecialTest("-inf", 1, false, false, true),
         SpecialTest("snan", 0, false, true, false),
         SpecialTest("-snan", 1, false, true, false),
-        SpecialTest("Fred", 0, true, false, false),
+        SpecialTest("Jack", 0, true, false, false),
         SpecialTest("+", 0, true, false, false),
         SpecialTest("-", 0, true, false, false),
         SpecialTest("nan0123", 0, true, false, false),
         SpecialTest("-nan0123", 1, true, false, false),
         SpecialTest("snan0123", 0, false, true, false),
+        SpecialTest("12+3", 0, true, false, false),
+        SpecialTest("1.2.3", 0, true, false, false),
+        SpecialTest("123.0E+7E+7", 0, true, false, false),
     ];
 
     foreach (el; nonspecialTestValues)
     {
+        writeln(el.val);
         auto d = Decimal(el.val);
         assert(d.coefficient == el.coefficient);
         assert(d.sign == el.sign);
@@ -211,6 +305,7 @@ unittest
 
     foreach (el; specialTestValues)
     {
+        writeln(el.val);
         auto d = Decimal(el.val);
         assert(d.qNaN == el.qNaN);
         assert(d.sNaN == el.sNaN);
