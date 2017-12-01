d2c78301a9bc25bb6a2e22e5ae1870c49e4b3c4c
Jack Stouffer <jack@jackstouffer.com>
2017-12-01 11:12:31 -0500

    String construction handles special values

--- source/stdxdecimal/package.d
+++ source/stdxdecimal/package.d
@@ -10,6 +10,7 @@
 module stdxdecimal;
 
 import std.stdio;
+import std.range.primitives;
 import core.stdc.stdlib;
 import std.traits;
 import std.bigint;
@@ -22,6 +23,7 @@ import std.conv;
  */
 struct Decimal
 {
+package:
     // 1 indicates that the number is negative or is the negative zero
     // and 0 indicates that the number is zero or positive.
     ubyte sign;
@@ -33,20 +35,104 @@ struct Decimal
     bool sNaN;
     bool isInfinite;
 
+public:
     this(T)(T val) if (isNumeric!T)
     {
 
     }
 
+    /**
+     * implements spec to-number
+     */
     this(string str)
     {
 
+        import std.algorithm.comparison : among;
+        import std.algorithm.searching : all, startsWith;
+        import std.utf : byCodeUnit;
+        import std.algorithm.comparison : equal;
+        import std.algorithm.iteration : map;
+        import std.ascii : toLower, isDigit;
+        import std.utf : byChar;
+
+        static bool asciiCmp(S1)(S1 a, string b)
+        {
+            return a.map!toLower.equal(b.byChar.map!toLower);
+        }
+
+        static bool charCheck(char a)
+        {
+            return isDigit(a) || a == 'E' || a == '-' || a == '+' || a == '.';
+        }
+
+        auto codeUnits = str.byCodeUnit;
+
+        if (codeUnits.empty)
+        {
+            qNaN = true;
+            return;
+        }
+
+        immutable frontResult = codeUnits.front;
+        if (frontResult == '+')
+        {
+            sign = 0;
+            codeUnits.popFront;
+        }
+        if (frontResult == '-')
+        {
+            sign = 1;
+            codeUnits.popFront;
+        }
+
+        if (codeUnits.empty)
+        {
+            sign = 0;
+            qNaN = true;
+            return;
+        }
+
+        if (codeUnits.among!((a, b) => asciiCmp(a.save, b))
+               ("inf", "infinity"))
+        {
+            isInfinite = true;
+            return;
+        }
+
+        if (codeUnits.save.map!toLower.startsWith("qnan") ||
+            codeUnits.save.map!toLower.startsWith("nan"))
+        {
+            qNaN = true;
+            return;
+        }
+
+        if (codeUnits.save.map!toLower.startsWith("snan"))
+        {
+            sNaN = true;
+            return;
+        }
+
+        if (!all!(a => charCheck(a))(codeUnits.save))
+        {
+            qNaN = true;
+            return;
+        }
     }
 
     bool opEquals(T)(T f) if (isFloatingPoint!T)
     {
         return false;
     }
+
+    //bool opEquals(T)(T d) if (is(T : Decimal))
+    //{
+    //    if ((isInfinite == d.isInfinite && sign == d.sign))
+    //    {
+    //        return true;
+    //    }
+
+    //    return false;
+    //}
 
     string toString()
     {
@@ -65,7 +151,70 @@ struct Decimal
     }
 }
 
-// construction
+// string construction
+unittest
+{
+    static struct Test
+    {
+        string val;
+        ubyte sign;
+        int coefficient;
+        long exponent;
+    }
+
+    static struct SpecialTest
+    {
+        string val;
+        ubyte sign;
+        bool qNaN;
+        bool sNaN;
+        bool isInfinite;
+    }
+
+    auto nonspecialTestValues = [
+        Test("1.0", 0, 10, -1),
+        Test("0E+7", 0, 0, 7),
+        Test("-0E-7", 1, 0, 7),
+        Test("0001.0000", 0, 10000, -4),
+        Test("-10.0004", 1, 100004, -4),
+        Test("+15", 0, 15, 0),
+        Test("-15", 1, 15, 0),
+    ];
+
+    auto specialTestValues = [
+        SpecialTest("NaN", 0, true, false, false),
+        SpecialTest("+nan", 0, true, false, false),
+        SpecialTest("-nan", 1, true, false, false),
+        SpecialTest("Infinite", 0, true, false, false),
+        SpecialTest("inf", 0, false, false, true),
+        SpecialTest("-inf", 1, false, false, true),
+        SpecialTest("snan", 0, false, true, false),
+        SpecialTest("-snan", 1, false, true, false),
+        SpecialTest("Fred", 0, true, false, false),
+        SpecialTest("+", 0, true, false, false),
+        SpecialTest("-", 0, true, false, false),
+        SpecialTest("nan0123", 0, true, false, false),
+        SpecialTest("snan0123", 0, false, true, false),
+    ];
+
+    //foreach (el; nonspecialTestValues)
+    //{
+    //    auto d = Decimal(el.val);
+    //    assert(d.coefficient == el.coefficient);
+    //    assert(d.sign == el.sign);
+    //    assert(d.exponent == el.exponent);
+    //}
+
+    foreach (el; specialTestValues)
+    {
+        auto d = Decimal(el.val);
+        assert(d.qNaN == el.qNaN);
+        assert(d.sNaN == el.sNaN);
+        assert(d.isInfinite == el.isInfinite);
+    }
+}
+
+// equals float
 unittest
 {
     //auto a = Decimal("1.0");
@@ -81,7 +230,9 @@ unittest
     //assert(d == 1.0);
 }
 
-unittest {
+// to string
+unittest
+{
     auto t = Decimal();
     t.sign = 0;
     t.coefficient = 2708;
@@ -98,6 +249,5 @@ unittest {
     t3.sign = 0;
     t3.coefficient = 9_888_555_555;
     t3.exponent = -4;
-    writeln(t3.toString());
     assert(t3.toString() == "988855.5555");
 }
